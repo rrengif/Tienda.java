2fde65db5ccd49e54c597af2005ddc1b0e3c6f51
rrengif <84943287+rrengif@users.noreply.github.com>
2021-08-08 23:06:08 -0500

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,21 @@
+import java.util.Collection;
+
+public class Producto {
+
+	private int codigo;
+
+	private string nombre;
+
+	private string tipo;
+
+	private date fecha;
+
+	private string nombreFabricante;
+
+	private int cantidadInventario;
+
+	private int precioUnitario;
+
+	private Collection<Tienda> tienda;
+
+}
---
+++ Tienda.java
@@ -0,0 +1,13 @@
+import java.util.Collection;
+
+public class Tienda {
+
+	private string nombre;
+
+	private string direccion;
+
+	private int telefono;
+
+	private Collection<Producto> producto;
+
+}
